5ee6841c88a2779d6e7966893fbec5ab5db0a0d9
liyang <1911178174@qq.com>
2017-11-07 19:49:17 +0800

    修改包路径

--- ldf-autoconfigure/ldf-auto-mapper/src/main/java/org/loushang/ldf/autoconfigure/MapperAutoConfiguration.java
+++ ldf-autoconfigure/ldf-auto-mapper/src/main/java/org/loushang/ldf/autoconfigure/MapperAutoConfiguration.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.loushang.ldf.mybatis.mapper.EntityMapper;
+import org.loushang.framework.mybatis.mapper.EntityMapper;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
@@ -22,32 +22,32 @@ import tk.mybatis.mapper.mapperhelper.MapperHelper;
 @AutoConfigureAfter(MybatisAutoConfiguration.class)
 public class MapperAutoConfiguration {
 
-    @Autowired
-    private List<SqlSessionFactory> sqlSessionFactoryList;
-    @Autowired
-    private ApplicationContext applicationContext;
-    @Autowired
-    private MapperProperties properties;
+	@Autowired
+	private List<SqlSessionFactory> sqlSessionFactoryList;
+	@Autowired
+	private ApplicationContext applicationContext;
+	@Autowired
+	private MapperProperties properties;
 
-    @PostConstruct
-    public void addPageInterceptor() {
-        MapperHelper mapperHelper = new MapperHelper();
-        mapperHelper.setConfig(properties);
-        if (properties.getMappers().size() > 0) {
-            for (Class<?> mapper : properties.getMappers()) {
-                //提前初始化MapperFactoryBean,注册mappedStatements
-                applicationContext.getBeansOfType(mapper);
-                mapperHelper.registerMapper(mapper);
-            }
-        } else {
-            //提前初始化MapperFactoryBean,注册mappedStatements
-            //applicationContext.getBeansOfType(Mapper.class);
-            //mapperHelper.registerMapper(Mapper.class);
-            applicationContext.getBeansOfType(EntityMapper.class);
-            mapperHelper.registerMapper(EntityMapper.class);
-        }
-        for (SqlSessionFactory sqlSessionFactory : sqlSessionFactoryList) {
-            mapperHelper.processConfiguration(sqlSessionFactory.getConfiguration());
-        }
-    }
+	@PostConstruct
+	public void addPageInterceptor() {
+		MapperHelper mapperHelper = new MapperHelper();
+		mapperHelper.setConfig(properties);
+		if (properties.getMappers().size() > 0) {
+			for (Class<?> mapper : properties.getMappers()) {
+				// 提前初始化MapperFactoryBean,注册mappedStatements
+				applicationContext.getBeansOfType(mapper);
+				mapperHelper.registerMapper(mapper);
+			}
+		} else {
+			// 提前初始化MapperFactoryBean,注册mappedStatements
+			// applicationContext.getBeansOfType(Mapper.class);
+			// mapperHelper.registerMapper(Mapper.class);
+			applicationContext.getBeansOfType(EntityMapper.class);
+			mapperHelper.registerMapper(EntityMapper.class);
+		}
+		for (SqlSessionFactory sqlSessionFactory : sqlSessionFactoryList) {
+			mapperHelper.processConfiguration(sqlSessionFactory.getConfiguration());
+		}
+	}
 }
