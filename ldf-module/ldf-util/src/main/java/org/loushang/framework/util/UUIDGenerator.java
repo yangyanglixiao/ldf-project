1a8ddb02c759b2c64c6e48f305038d51f623bd0f
liyang <1911178174@qq.com>
2017-11-07 15:51:20 +0800

    添加工具类项目

---
+++ ldf-module/ldf-util/src/main/java/org/loushang/framework/util/UUIDGenerator.java
@@ -0,0 +1,36 @@
+package org.loushang.framework.util;
+
+import java.util.UUID;
+
+/**
+ * @author
+ *
+ */
+public class UUIDGenerator {
+    /** 
+     * 获得一个UUID 
+     * @return String UUID 
+     */ 
+    public static String getUUID(){ 
+        String s = UUID.randomUUID().toString(); 
+        //去掉“-”符号 
+        s = s.replace("-", "");
+        return s;
+    } 
+    /** 
+     * 获得指定数目的UUID 
+     * @param number int 需要获得的UUID数量 
+     * @return String[] UUID数组 
+     */ 
+    public static String[] getUUID(int number){ 
+        if(number < 1){ 
+            return null; 
+        } 
+        String[] ss = new String[number]; 
+        for(int i=0;i<number;i++){ 
+            ss[i] = getUUID(); 
+        } 
+        return ss; 
+    } 
+
+}
