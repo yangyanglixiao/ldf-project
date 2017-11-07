31e1e115e39fbb2c0d162b9e90aac8f2d8cc3980
liyang <1911178174@qq.com>
2017-11-07 13:46:29 +0800

    添加通用mapper的接口

--- ldf-module/ldf-mapper/src/main/java/org/loushang/ldf/mybatis/mapper/EntityMapper.java
+++ ldf-module/ldf-mapper/src/main/java/org/loushang/ldf/mybatis/mapper/EntityMapper.java
@@ -3,24 +3,21 @@ package org.loushang.ldf.mybatis.mapper;
 import org.loushang.ldf.mybatis.mapper.api.BatchDeleteMapper;
 import org.loushang.ldf.mybatis.mapper.api.BatchInsertMapper;
 import org.loushang.ldf.mybatis.mapper.api.DeleteByPrimaryKeyMapper;
+import org.loushang.ldf.mybatis.mapper.api.DeleteMapper;
+import org.loushang.ldf.mybatis.mapper.api.ExistsWithPrimaryKeyMapper;
 import org.loushang.ldf.mybatis.mapper.api.GetAllMapper;
 import org.loushang.ldf.mybatis.mapper.api.GetByPrimaryKeyMapper;
+import org.loushang.ldf.mybatis.mapper.api.GetMapper;
 import org.loushang.ldf.mybatis.mapper.api.GetTotalCountMapper;
 import org.loushang.ldf.mybatis.mapper.api.InsertMapper;
 import org.loushang.ldf.mybatis.mapper.api.QueryMapper;
 import org.loushang.ldf.mybatis.mapper.api.UpdateByPrimaryKeyMapper;
 
-public interface EntityMapper<T> extends 
-	GetAllMapper<T>, 
-	GetByPrimaryKeyMapper<T>,
-	QueryMapper<T>,
-	GetTotalCountMapper<T>,
-	InsertMapper<T>, 
-	UpdateByPrimaryKeyMapper<T>, 
-	BatchInsertMapper<T>,
-	BatchDeleteMapper<T>,
-	DeleteByPrimaryKeyMapper<T>/*,
-	BatchUpdateMapper<T>,
-	BatchSaveMapper<T>*/ {
+public interface EntityMapper<T>
+		extends DeleteMapper<T>, ExistsWithPrimaryKeyMapper<T>, GetMapper<T>, GetAllMapper<T>, GetByPrimaryKeyMapper<T>,
+		QueryMapper<T>, GetTotalCountMapper<T>, InsertMapper<T>, UpdateByPrimaryKeyMapper<T>, BatchInsertMapper<T>,
+		BatchDeleteMapper<T>, DeleteByPrimaryKeyMapper<T>/*
+															 * , BatchUpdateMapper<T>, BatchSaveMapper<T>
+															 */ {
 
 }
