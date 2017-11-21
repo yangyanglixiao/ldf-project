c76f532505b25f22d6aeee9d07b26a0169ec638f
chenglongxie <chenglongxie@outlook.com>
2017-11-21 11:14:52 +0800

    拆分适配类

--- ldf-module/ldf-util/src/main/java/org/loushang/framework/util/RestRequestUtils.java
+++ ldf-module/ldf-util/src/main/java/org/loushang/framework/util/RestRequestUtils.java
@@ -1,13 +1,16 @@
 package org.loushang.framework.util;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.lang.reflect.Method;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
 public class RestRequestUtils {
-
+	
+	private static Logger logger = LoggerFactory.getLogger(RestRequestUtils.class);
+	
 	private static RestTemplate restTemplate = new RestTemplate();
 
 	/**
@@ -17,20 +20,22 @@ public class RestRequestUtils {
 	 * @param uriVariables:参数
 	 * @return
 	 */
-	public static <T> T getForObject(String url, Class<T> responseType, Map<String, ?> uriVariables) {
-		url = isK8s(url);
+	public static <T> T get(String url, Class<T> responseType, Map<String, ?> uriVariables) {
+		url = adaptor(url);
 		return restTemplate.getForObject(url, responseType, uriVariables);
 	}
+	
 	/**
 	 * 
 	 * @param url,ex:http://localhost:8080/demo/employees
 	 * @param responseType:返回值类型,ex:String.class
 	 * @return
 	 */
-	public static <T> T getForObject(String url, Class<T> responseType) {
-		url = isK8s(url);
+	public static <T> T get(String url, Class<T> responseType) {
+		url = adaptor(url);
 		return restTemplate.getForObject(url, responseType);
 	}
+	
 	/**
 	 * POST
 	 * @param url
@@ -38,8 +43,8 @@ public class RestRequestUtils {
 	 * @param responseType
 	 * @return
 	 */
-	public static <T> T postForObject(String url, Object request, Class<T> responseType) {
-		url = isK8s(url);
+	public static <T> T post(String url, Object request, Class<T> responseType) {
+		url = adaptor(url);
 		return restTemplate.postForObject(url, request, responseType);
 	}
 
@@ -50,59 +55,52 @@ public class RestRequestUtils {
 	 * @param uriVariables
 	 */
 	public static void put(String url, Object request, Map<String, ?> uriVariables) {
-		url = isK8s(url);
+		url = adaptor(url);
 		restTemplate.put(url, request, uriVariables);
 	}
+	
 	/**
 	 * PUT
 	 * @param url
 	 * @param request
 	 */
 	public static void put(String url, Object request) {
-		url = isK8s(url);
+		url = adaptor(url);
 		restTemplate.put(url, request);
 	}
+	
 	/**
 	 * DELETE
 	 * @param url
 	 * @param uriVariables
 	 */
 	public static void delete(String url, Map<String, ?> uriVariables) {
-		url = isK8s(url);
+		url = adaptor(url);
 		restTemplate.delete(url, uriVariables);
 	}
+	
 	/**
 	 * DELETE
 	 * @param url
 	 */
 	public static void delete(String url) {
-		url = isK8s(url);
+		url = adaptor(url);
 		restTemplate.delete(url);
 	}
+
 	/**
 	 * url适配
 	 * @param url
 	 * @return
 	 */
-	private static String isK8s(String urlStr){
-		if(urlStr!=null&&!"".equals(urlStr)){
-			try {
-				URL url = new URL(urlStr);
-				String Protocol = url.getProtocol();
-				String nameService = url.getHost();
-				String path = url.getPath();
-				if(nameService!=null&&!"".equals(nameService)){
-					String service_host = System.getenv(nameService.toUpperCase()+"_SERVICE_HOST");
-					String service_port = System.getenv(nameService.toUpperCase()+"_SERVICE_PORT");
-					if(null!=service_host&&!"".equals(service_host)&&null!=service_port&&!"".equals(service_port)){
-						urlStr = Protocol+ "://" + service_host + ":" + service_port + "/" +nameService + path;
-					}
-				}
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			}
+	private static String adaptor(String url){
+		try {
+			Class<?> class1 = Class.forName("org.loushang.framework.util.RestRequestAdaptor");
+			Method method = class1.getMethod("get", String.class);
+			url = (String) method.invoke(class1, url);
+		} catch (Exception e) {
+			logger.error("org.loushang.framework.util.RestRequestAdaptor 类不存在,不进行适配");
 		}
-		return urlStr;
-		
+		return url;
 	}
 }
