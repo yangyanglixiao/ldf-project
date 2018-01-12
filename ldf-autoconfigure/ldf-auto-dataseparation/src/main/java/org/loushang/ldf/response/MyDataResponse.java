dcd2cb43c0d930e2f0a4f7a2abd12487f56ec274
liyang_rj <liyang_rj@inspur.com>
2018-01-12 10:55:23 +0800

    更新注释

--- ldf-autoconfigure/ldf-auto-dataseparation/src/main/java/org/loushang/ldf/response/MyDataResponse.java
+++ ldf-autoconfigure/ldf-auto-dataseparation/src/main/java/org/loushang/ldf/response/MyDataResponse.java
@@ -1,83 +1,83 @@
 package org.loushang.ldf.response;
 
 /**
- * Title: 统一响应结构 
+ * Title: 统一响应结构
  * Description:使用REST框架实现前后端分离架构，我们需要首先确定返回的JSON响应结构是统一的，
  * 也就是说，每个REST请求将返回相同结构的JSON响应结构。不妨定义一个相对通用的JSON响应结构，其
  * 中包含两部分：元数据与返回值，其中，元数据表示操作是否成功与返回值消息等，返回值对应服务端方法所返回的数据。
- * { "meta": { "success": true, "message": "ok" }, "data": ... }
+ * { "meta": { "success": true, "message": "ok","exception": null }, "data": ... }
  */
 public class MyDataResponse {
 
-	private static final String OK = "ok";
-	private static final String ERROR = "error";
+    private static final String OK = "ok";
+    private static final String ERROR = "error";
 
-	private Meta meta;     // 元数据
-	private Object data;   // 响应内容
+    private Meta meta;     // 元数据
+    private Object data;   // 响应内容
 
-	public MyDataResponse success() {
-		this.meta = new Meta(true, OK);
-		return this;
-	}
+    public MyDataResponse success() {
+        this.meta = new Meta(true, OK);
+        return this;
+    }
 
-	public MyDataResponse success(Object data) {
-		this.meta = new Meta(true, OK);
-		this.data = data;
-		return this;
-	}
+    public MyDataResponse success(Object data) {
+        this.meta = new Meta(true, OK);
+        this.data = data;
+        return this;
+    }
 
-	public MyDataResponse failure() {
-		this.meta = new Meta(false, ERROR);
-		return this;
-	}
+    public MyDataResponse failure() {
+        this.meta = new Meta(false, ERROR);
+        return this;
+    }
 
-	public MyDataResponse failure(String message, String exception) {
-		this.meta = new Meta(false, message,exception);
-		return this;
-	}
+    public MyDataResponse failure(String message, String exception) {
+        this.meta = new Meta(false, message, exception);
+        return this;
+    }
 
-	public Meta getMeta() {
-		return meta;
-	}
+    public Meta getMeta() {
+        return meta;
+    }
 
-	public Object getData() {
-		return data;
-	}
+    public Object getData() {
+        return data;
+    }
 
-	/**
-	 * Title: 请求元数据
-	 */
-	public class Meta {
+    /**
+     * Title: 请求元数据
+     */
+    public class Meta {
 
-		private boolean success;
-		private String message;
-		private String exception;
+        private boolean success;
+        private String message;
+        private String exception;
 
-		public Meta(boolean success) {
-			this.success = success;
-		}
+        public Meta(boolean success) {
+            this.success = success;
+        }
 
-		public Meta(boolean success, String message) {
-			this.success = success;
-			this.message = message;
-		}
+        public Meta(boolean success, String message) {
+            this.success = success;
+            this.message = message;
+        }
 
-		public Meta(boolean success, String message, String exception) {
-			this.success = success;
-			this.message = message;
-			this.exception = exception;
-		}
+        public Meta(boolean success, String message, String exception) {
+            this.success = success;
+            this.message = message;
+            this.exception = exception;
+        }
 
-		public boolean isSuccess() {
-			return success;
-		}
+        public boolean isSuccess() {
+            return success;
+        }
 
-		public String getMessage() {
-			return message;
-		}
+        public String getMessage() {
+            return message;
+        }
 
-		public String getException() {
-			return exception;
-		}
-	}
+        public String getException() {
+            return exception;
+        }
+    }
 }
